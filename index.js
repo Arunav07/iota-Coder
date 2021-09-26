b03df2149b8e2bbfe05e2d0aaebb06094e6580fb
Arunav07 <arunav014.ac@gmail.com>
2021-09-26 11:33:00 +0530

    update 3

--- index.js
+++ index.js
@@ -13,6 +13,20 @@ $('.lessGo').mouseleave(function () {
     $('.lessGo').removeClass("DesignerButton");
 })
 
+var preloader = document.getElementById("loading");
+var mainContainer = document.getElementById("main1");
+function LoadingFunction() {
+}
+ 
+$(document).ready(function LoadingFunction() {
+    $('#loading').show();
+    $('#loading').delay(5000).fadeOut(100);
+    $("#main1").hide();
+    $("#main1").delay(5000).fadeIn(100);  
+
+})
+
+
 
 //GSAP
 gsap.registerPlugin(MotionPathPlugin, TextPlugin);
@@ -20,7 +34,7 @@ gsap.registerPlugin(MotionPathPlugin, TextPlugin);
 
 
 gsap.set('.main', {position:'fixed', background:'#fff', width:'100%', maxWidth:'100vw', top:0, left:'50%', x:'-50%'})
-gsap.set('.scrollDist', { width: '100%', height: '200%' })
+gsap.set('.scrollDist', { width: '100%', height: '400%' })
 let tl = gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:2 }})
     .fromTo('.sky', {y:0},{y:-200}, 0)
     tl.fromTo('.cloud1', {y:100},{y:-800}, 0)
@@ -29,5 +43,8 @@ let tl = gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', e
     tl.to('.mountBg', {y:-10},{y:-100}, 0)
 tl.to('.mountMg', { y: -30 }, { y: -250 }, 0)
 tl.to('.mountFg', { y: -600, duration: 3, ease: none,  }, 0)
-    
-.fromTo('#clipPathReveal', {y: 0},{y:100}, 1)
+   
+
+
+
+gsap.to(".one", {duration: 1, background: "#f0f"}, 1)
