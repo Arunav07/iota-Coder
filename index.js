63e4bcdb39d4de6d9b2ed20e3b9ee97cb5d778f6
Arunav07 <arunav014.ac@gmail.com>
2021-09-26 08:06:22 +0530

    update1

--- index.js
+++ index.js
@@ -6,17 +6,29 @@ $('.darkMode').click(function () {
     $('*').css("transition", "0.1s");
 });
 
-gsap.set('.main', {position:'fixed', background:'#fff', width:'100%', maxWidth:'100vw', height:'100%', top:0, left:'50%', x:'-50%'})
-gsap.set('.scrollDist', { width: '100%', height: '200%' })
-gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:1}})
-    .fromTo('.sky', {y:0},{y:-200}, 0)
-    .fromTo('.cloud1', {y:100},{y:-800}, 0)
-    .fromTo('.cloud2', {y:-150},{y:-500}, 0)
-    .fromTo('.cloud3', {y:-50},{y:-650}, 0)
-    .fromTo('.mountBg', {y:-10},{y:-100}, 0)
-    .fromTo('.mountMg', {y:-30},{y:-250}, 0)
-    .fromTo('.mountFg', {y:-50},{y:-600}, 0)
+$('.lessGo').mouseenter(function () {
+    $('.lessGo').addClass("DesignerButton");
+})
+$('.lessGo').mouseleave(function () {
+    $('.lessGo').removeClass("DesignerButton");
+})
 
-$('#arrowBtn').on('mouseenter', (e)=>{ gsap.to('.arrow', {y:10, duration:0.5, ease:'back.inOut(3)', overwrite:'auto'}); })
-$('#arrowBtn').on('mouseleave', (e)=>{ gsap.to('.arrow', {y:0, duration:0.3, ease:'power3.out', overwrite:'auto'}); })
-$('#arrowBtn').on('click', (e)=>{ gsap.to(window, {scrollTo:innerHeight, duration:1.5, ease:'power1.inOut'}); }) // scrollTo requires the ScrollTo plugin (not to be confused w/ ScrollTrigger)
+
+//GSAP
+gsap.registerPlugin(MotionPathPlugin,TextPlugin);
+
+gsap.to('#Counting', { duration: 4, text: "2", ease: none });
+
+
+gsap.set('.main', {position:'fixed', background:'#fff', width:'100%', maxWidth:'100vw', top:0, left:'50%', x:'-50%'})
+gsap.set('.scrollDist', { width: '100%', height: '200%' })
+let tl = gsap.timeline({scrollTrigger:{trigger:'.scrollDist', start:'top top', end:'bottom bottom', scrub:2 }})
+    .fromTo('.sky', {y:0},{y:-200}, 0)
+    tl.fromTo('.cloud1', {y:100},{y:-800}, 0)
+    tl.fromTo('.cloud2', {y:-150},{y:-500}, 0)
+    tl.fromTo('.cloud3', {y:0},{y:-650}, 0)
+    tl.to('.mountBg', {y:-10},{y:-100}, 0)
+    tl.to('.mountMg', {y:-30},{y:-250}, 0)
+tl.to('.mountFg', { y: -600, duration: 3, ease: none,  }, 0)
+    
+
